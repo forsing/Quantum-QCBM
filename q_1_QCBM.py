b15e21417d28949caff0ccd1478c659c790941a1
ForsIng <forsing@gmail.com>
2026-03-19 18:37:47 +0100

    Update q_1_QCBM.py

--- q_1_QCBM.py
+++ q_1_QCBM.py
@@ -18,8 +18,8 @@ np.random.seed(SEED)
 random.seed(SEED)
 algorithm_globals.random_seed = SEED
 
-CSV_DRAWN = "/Users/4c/Desktop/GHQ/data/loto7hh_4582_k22.csv"
-CSV_ALL   = "/Users/4c/Desktop/GHQ/data/kombinacijeH_39C7.csv"
+CSV_DRAWN = "/data/loto7hh_4582_k22.csv"
+CSV_ALL   = "/data/kombinacijeH_39C7.csv"
 
 MIN_VAL = [1, 2, 3, 4, 5, 6, 7]
 MAX_VAL = [33, 34, 35, 36, 37, 38, 39]
@@ -169,7 +169,7 @@ Graf svih kombinacija: /Users/4c/Desktop/GHQ/data/kombinacijeH_39C7.csv
 
 ==================================================
 Predikcija (QCBM, deterministicki, seed=39):
-[1, 6, 11, 24, 26, 30, 35]
+[1, 6, x, y, z, 30, 35]
 ==================================================
 """
 
